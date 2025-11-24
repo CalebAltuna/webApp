274e043f4ff45f470edf0c00765f5bbb271db5e8
CalebAltuna <web24cvalls@imaltuna.com>
2025-11-24 18:44:16 +0100

    testing -first commit

---
+++ src/app/home/home.ts
@@ -0,0 +1,16 @@
+// src/app/home/home.component.ts
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule],       // necesario para ngIf, ngFor, etc.
+  template: `
+    <h1>Bienvenido</h1>
+    <p>My first component</p>
+    <div>
+    </div>
+  `,
+  styleUrls: ['./home.scss'],    // siempre en array
+})
+export class HomeComponent {}
