c86f944226be12f67ef66d840e380a9d73f831f1
CalebAltuna <web24@cvalls@imaltuna.com>
2025-12-04 08:10:28 +0100

    test 01, savings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
 
 // jfasf
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
